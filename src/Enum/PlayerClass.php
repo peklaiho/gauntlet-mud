f59d2b2d9b6d9fc0e0de1aee4905725061cb13b3
Pekka Laiho <pekka.i.laiho@gmail.com>
2025-02-02 07:02:50 +0200

    Rename Healer class to Shaman. It sounds a little more interesting than a generic healer.

--- src/Enum/PlayerClass.php
+++ src/Enum/PlayerClass.php
@@ -9,19 +9,19 @@ namespace Gauntlet\Enum;
 
 enum PlayerClass: string
 {
-    case Healer = 'healer';
     case Mage = 'mage';
     case Rogue = 'rogue';
+    case Shaman = 'shaman';
     case Warrior = 'warrior';
 
     public static function parse(string $val): ?PlayerClass
     {
-        if (str_starts_with_case($val, 'h')) {
-            return PlayerClass::Healer;
-        } elseif (str_starts_with_case($val, 'm')) {
+        if (str_starts_with_case($val, 'm')) {
             return PlayerClass::Mage;
         } elseif (str_starts_with_case($val, 'r')) {
             return PlayerClass::Rogue;
+        } elseif (str_starts_with_case($val, 's')) {
+            return PlayerClass::Shaman;
         } elseif (str_starts_with_case($val, 'w')) {
             return PlayerClass::Warrior;
         }
@@ -31,18 +31,18 @@ enum PlayerClass: string
 
     public static function listChoices(): string
     {
-        return '(h)ealer, (m)age, (r)ogue, (w)arrior';
+        return '(m)age, (r)ogue, (s)haman, (w)arrior';
     }
 
     public static function infoText(): array
     {
         return [
-            '(H)ealer uses healing magic to cure wounds and other ailments.' .
-                ' Healer also wields offensive magic that is particularly effective against evil foes.',
             '(M)age is the master of offensive magic that focuses on elements such as fire and cold.' .
                 ' Mage can also inflict curses upon their enemies to weaken them.',
             '(R)ogue is the master of subterfuge, preferring to stay in the shadows and ambush' .
                 ' unsuspecting enemies. Rogue also uses poison to weaken their enemies.',
+            '(S)haman uses healing magic to cure wounds and other ailments.' .
+                ' Shaman also wields offensive magic that is particularly effective against evil foes.',
             '(W)arrior is the master of physical combat. Warriors overwhelm their enemies with brute force.' .
                 ' They can wield the deadliest weapons and gain additional attacks.',
         ];
